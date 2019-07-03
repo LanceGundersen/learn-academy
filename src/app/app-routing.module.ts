00395df77c53a36c7572e9cd31aa88589ab22abb
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2019-07-03 11:28:46 -0400

    Generate initial outline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { LogonComponent } from './logon/logon.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LogonComponent, pathMatch: 'full' },
+  // { path: '/dashboard', pathMatch: 'full' },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogonComponent } from './logon/logon.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CourseComponent } from './course/course.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogonComponent,
+    DashboardComponent,
+    CourseComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
